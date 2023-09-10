4573d46658a0cddb9ba4556472bd7b999a9594ce
Jashdedhia3 <114570502+Jashdedhia3@users.noreply.github.com>
2023-09-10 14:00:18 +0530

    Add files via upload

---
+++ studentmgm/src/main/java/com/greatlearning/studentmgm/StudentmgmApplication.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentmgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmgmApplication.class, args);
+	}
+
+}
---
+++ studentmgm/src/test/java/com/greatlearning/studentmgm/StudentmgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatlearning.studentmgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
